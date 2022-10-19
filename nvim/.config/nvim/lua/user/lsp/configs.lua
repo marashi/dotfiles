1b3cbcbaa4c30060856165b2b9a04fcf43d9455d
Matt Marashi <matt.marashi@gmail.com>
2022-10-19 16:14:02 -0400

    add angularls to list of lsp servers

--- nvim/.config/nvim/lua/user/lsp/configs.lua
+++ nvim/.config/nvim/lua/user/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "tsserver", "jsonls", "sumneko_lua" }
+local servers = { "tsserver", "jsonls", "sumneko_lua", "angularls" }
 
 lsp_installer.setup({
 	ensure_installed = servers,
