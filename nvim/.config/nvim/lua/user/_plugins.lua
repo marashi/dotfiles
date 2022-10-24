c85d63a62ba604fc7a759708890703d4f2536cc8
Matt Marashi <matt.marashi@gmail.com>
2022-10-24 18:39:08 -0400

    exclude .git from telescope search

--- nvim/.config/nvim/lua/user/_plugins.lua
+++ nvim/.config/nvim/lua/user/_plugins.lua
@@ -81,7 +81,6 @@ return packer.startup(function(use)
   use "neovim/nvim-lspconfig"
   use "williamboman/mason.nvim"
   use "williamboman/mason-lspconfig.nvim"
-  -- use "williamboman/nvim-lsp-installer"
   use "jose-elias-alvarez/null-ls.nvim"
   use "RRethy/vim-illuminate"
 
--- nvim/.config/nvim/lua/user/telescope.lua
+++ nvim/.config/nvim/lua/user/telescope.lua
@@ -7,7 +7,7 @@ local actions = require "telescope.actions"
 
 telescope.setup {
   defaults = {
-
+    file_ignore_patterns = {".git/"},
     prompt_prefix = " ",
     selection_caret = " ",
     path_display = { "smart" },
