8d2d623a31677b2ac8fbaf5b8985e5dfc820558d
Matt Marashi <matt@Matts-MacBook-Pro.local>
2023-02-10 15:41:49 -0500

    fix: update indent plugin config

--- nvim/.config/nvim/lua/user/indent-blankline.lua
+++ nvim/.config/nvim/lua/user/indent-blankline.lua
@@ -3,8 +3,16 @@ if not status_ok then
 	return
 end
 
+vim.cmd([[highlight IndentBlanklineIndent guifg=#86abdc gui=nocombine]])
+vim.cmd([[highlight IndentBlanklineContextSpaceChar guisp=#dbc074 gui=underline]])
+
 indent.setup({
 	-- for example, context is off by default, use this to turn it on
-	show_current_context = false,
+	show_current_context = true,
 	show_current_context_start = false,
+	char_highlight_list = {
+		"IndentBlanklineIndent",
+	},
 })
+
+vim.g.indent_blankline_show_current_context_start = false
