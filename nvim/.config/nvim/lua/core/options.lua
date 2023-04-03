13fe93e47230b460d5efd2701dd2c49a60e25c5f
Matt Marashi <matt@Matts-MacBook-Pro.local>
2023-04-03 00:33:14 -0400

    fix: set folding based on treesitter

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -33,7 +33,8 @@ local options = {
 	scrolloff = 8, -- is one of my fav
 	sidescrolloff = 8,
 	guifont = "monospace:h17", -- the font used in graphical neovim applications
-	foldmethod = "syntax",
+	foldmethod = "expr",
+	foldexpr = "nvim_treesitter#foldexpr()",
 	foldcolumn = "1",
 	foldlevelstart = 99, -- open fold on file open
 }
