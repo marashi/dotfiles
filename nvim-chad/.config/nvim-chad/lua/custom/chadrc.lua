e55e9bbe447207ff1238e884ba2f442a92340e72
Matt Marashi <matt@marashi.dev>
2023-06-16 13:21:03 -0400

    remove extra comment

--- nvim-chad/.config/nvim-chad/lua/custom/chadrc.lua
+++ nvim-chad/.config/nvim-chad/lua/custom/chadrc.lua
@@ -1,36 +1,35 @@
----@type ChadrcConfig
-vim.cmd [[
+vim.cmd([[
   highlight Visual gui=reverse
-]]
+]])
 
 local M = {}
 M.ui = {
-  theme = "nightowl",
-  statusline = {
-    theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
-  },
-  nvdash = {
-    load_on_startup = true,
+	theme = "nightowl",
+	statusline = {
+		theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
+	},
+	nvdash = {
+		load_on_startup = true,
 
-    header = {
-      [[      .            .      ]],
-      [[    .,;'           :,.    ]],
-      [[  .,;;;,,.         ccc;.  ]],
-      [[.;c::::,,,'        ccccc: ]],
-      [[.::cc::,,,,,.      cccccc.]],
-      [[.cccccc;;;;;;'     llllll.]],
-      [[.cccccc.,;;;;;;.   llllll.]],
-      [[.cccccc  ';;;;;;'  oooooo.]],
-      [['lllllc   .;;;;;;;.oooooo']],
-      [['lllllc     ,::::::looooo']],
-      [['llllll      .:::::lloddd']],
-      [[.looool       .;::coooodo.]],
-      [[  .cool         'ccoooc.  ]],
-      [[    .co          .:o:.    ]],
-      [[      .           .'      ]],
-      [[                          ]],
-    },
-  },
+		header = {
+			[[      .            .      ]],
+			[[    .,;'           :,.    ]],
+			[[  .,;;;,,.         ccc;.  ]],
+			[[.;c::::,,,'        ccccc: ]],
+			[[.::cc::,,,,,.      cccccc.]],
+			[[.cccccc;;;;;;'     llllll.]],
+			[[.cccccc.,;;;;;;.   llllll.]],
+			[[.cccccc  ';;;;;;'  oooooo.]],
+			[['lllllc   .;;;;;;;.oooooo']],
+			[['lllllc     ,::::::looooo']],
+			[['llllll      .:::::lloddd']],
+			[[.looool       .;::coooodo.]],
+			[[  .cool         'ccoooc.  ]],
+			[[    .co          .:o:.    ]],
+			[[      .           .'      ]],
+			[[                          ]],
+		},
+	},
 }
 M.plugins = "custom.plugins"
 
