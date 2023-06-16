2bfb15de28af7d7e432ad2c906dea07bf80de8db
Matt Marashi <matt@marashi.dev>
2023-06-16 12:17:59 -0400

    add root_dir for angularls

--- nvim-chad/.config/nvim-chad/lua/custom/configs/lspconfig.lua
+++ nvim-chad/.config/nvim-chad/lua/custom/configs/lspconfig.lua
@@ -1,12 +1,21 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 local servers = { "lua_ls", "cssls", "html", "tsserver", "jsonls", "angularls", "dockerls", "rust_analyzer", "eslint" }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+	if lsp == "angularls" then
+		local util = require("lspconfig.util")
+		lspconfig[lsp].setup({
+			on_attach = on_attach,
+			capabilities = capabilities,
+			root_dir = util.root_pattern("angular.json", "project.json"), -- libs do not have angular.json
+		})
+	else
+		lspconfig[lsp].setup({
+			on_attach = on_attach,
+			capabilities = capabilities,
+		})
+	end
 end
