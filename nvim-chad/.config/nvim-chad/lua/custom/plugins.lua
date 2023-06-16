9dac8aafd5c0f26313874bf9f808c43c07d24aca
Matt Marashi <matt@marashi.dev>
2023-06-16 11:44:04 -0400

    add angular language server

--- nvim-chad/.config/nvim-chad/lua/custom/plugins.lua
+++ nvim-chad/.config/nvim-chad/lua/custom/plugins.lua
@@ -1,62 +1,63 @@
 local plugins = {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        -- defaults
-        "vim",
-        "lua",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				-- defaults
+				"vim",
+				"lua",
 
-        -- web dev
-        "html",
-        "css",
-        "javascript",
-        "typescript",
-        "tsx",
-        "json",
-        -- "vue", "svelte",
+				-- web dev
+				"html",
+				"css",
+				"javascript",
+				"typescript",
+				"tsx",
+				"json",
+				-- "vue", "svelte",
 
-        -- low level
-        "rust",
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "lua-language-server",
-        "html-lsp",
-        "css-lsp",
-        "eslint-lsp",
-        "json-lsp",
-        "rust-analyzer",
-        "typescript-language-server",
-        "prettier",
-        "stylua",
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "jose-elias-alvarez/null-ls.nvim",
-      config = function()
-        require "custom.configs.null-ls"
-      end,
-    },
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end,
-  },
+				-- low level
+				"rust",
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"lua-language-server",
+				"html-lsp",
+				"css-lsp",
+				"eslint-lsp",
+				"json-lsp",
+				"rust-analyzer",
+				"typescript-language-server",
+				"angular-language-server",
+				"prettier",
+				"stylua",
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"jose-elias-alvarez/null-ls.nvim",
+			config = function()
+				require("custom.configs.null-ls")
+			end,
+		},
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end,
+	},
 }
 
 return plugins
