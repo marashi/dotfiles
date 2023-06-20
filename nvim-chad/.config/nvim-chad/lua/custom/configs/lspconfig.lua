d593e5aa7edf31a48efdceea5f44a9cde66b99c5
Matt Marashi <matt@marashi.dev>
2023-06-20 00:57:43 -0400

    add organize import on tsserver

--- nvim-chad/.config/nvim-chad/lua/custom/configs/lspconfig.lua
+++ nvim-chad/.config/nvim-chad/lua/custom/configs/lspconfig.lua
@@ -4,6 +4,15 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require("lspconfig")
 local servers = { "lua_ls", "cssls", "html", "tsserver", "jsonls", "angularls", "dockerls", "rust_analyzer", "eslint" }
 
+local function organize_imports()
+	local params = {
+		command = "_typescript.organizeImports",
+		arguments = { vim.api.nvim_buf_get_name(0) },
+		title = "",
+	}
+	vim.lsp.buf.execute_command(params)
+end
+
 for _, lsp in ipairs(servers) do
 	if lsp == "angularls" then
 		local util = require("lspconfig.util")
@@ -11,6 +20,17 @@ for _, lsp in ipairs(servers) do
 			on_attach = on_attach,
 			capabilities = capabilities,
 			root_dir = util.root_pattern("angular.json", "project.json"), -- libs do not have angular.json
+		})
+	elseif lsp == "tsserver" then
+		lspconfig[lsp].setup({
+			on_attach = on_attach,
+			capabilities = capabilities,
+			commands = {
+				OrganizeImports = {
+					organize_imports,
+					description = "Organize Imports",
+				},
+			},
 		})
 	else
 		lspconfig[lsp].setup({
---
+++ nvim-chad/.config/nvim-chad/lua/custom/mappings.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+M.lsp = {
+	n = {
+		["<leader>lo"] = { "<cmd> OrganizeImports <CR>", "Organize Imports" },
+	},
+}
+
+return M
